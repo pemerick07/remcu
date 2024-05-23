3a1991af70c5698e1b778b703cdbc594278c02e7
Emre Sunar <134863869+emrxv@users.noreply.github.com>
2024-05-23 01:45:16 -0400

    new gcc

--- platform/raspberry_pi_armv8_bcm2710.cmake
+++ platform/raspberry_pi_armv8_bcm2710.cmake
@@ -14,10 +14,10 @@ endif()
 set(RASPBERRY_TOOL_PATH $ENV{RASPBERRY_TOOL_PATH})
 
 # Define the cross compiler locations
-SET(CMAKE_C_COMPILER   ${RASPBERRY_TOOL_PATH}/cmake/armv8-rpi3-linux-gnueabihf/arm-linux-gnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER ${RASPBERRY_TOOL_PATH}/cmake/armv8-rpi3-linux-gnueabihf/arm-linux-gnueabihf-g++)
+SET(CMAKE_C_COMPILER   ${RASPBERRY_TOOL_PATH}/armv8-rpi3-linux-gnueabihf/bin/armv8-rpi3-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER ${RASPBERRY_TOOL_PATH}/armv8-rpi3-linux-gnueabihf/bin/armv8-rpi3-linux-gnueabihf-g++)
 # Define the sysroot path for the RaspberryPi distribution in our tools folder 
-SET(CMAKE_FIND_ROOT_PATH ${RASPBERRY_TOOL_PATH}/armv8-rpi3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/)
+SET(CMAKE_FIND_ROOT_PATH ${RASPBERRY_TOOL_PATH}/armv8-rpi3-linux-gnueabihf/armv8-rpi3-linux-gnueabihf/sysroot/)
 
 SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
 SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
@@ -36,7 +36,7 @@ include(${REMCU_VM_PATH}/cmake/common_linux_flags.cmake)
 
 set(MCU_INCLUDE_MK_FILE ${REMCU_VM_PATH}/mcu_utils/linux.mk)
 
-# set(STRIP_COMMAND ${RASPBERRY_TOOL_PATH}/armv8-rpi3-linux-gnueabihf/bin/arm-linux-gnueabihf-strip)
+# set(STRIP_COMMAND ${RASPBERRY_TOOL_PATH}/armv8-rpi3-linux-gnueabihf/bin/armv8-rpi3-linux-gnueabihf-strip)
 
 set(TARGET_REMCU_OS_NAME Raspbian_armv8)
 
