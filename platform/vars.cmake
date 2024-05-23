bdf4cbeaa14cfd903dc5cf14a29ea4b8899260ae
Emre Sunar <134863869+emrxv@users.noreply.github.com>
2024-05-23 01:55:06 -0400

    execute process

--- platform/vars.cmake
+++ platform/vars.cmake
@@ -1,4 +1,3 @@
-
 if(DEFINED ENV{LLVM_ADIN_PATH})
   set(LLVM_ADIN_PATH $ENV{LLVM_ADIN_PATH})
 else()
@@ -10,7 +9,7 @@ find_program(RESULT_FIND_OPT
   PATHS ${LLVM_ADIN_PATH}
   NO_DEFAULT_PATH
   REQUIRED
-  )
+)
 
 include(CMakePrintHelpers)
 cmake_print_variables(RESULT_FIND_OPT)
@@ -23,23 +22,33 @@ add_custom_command(
   OUTPUT opt_file_find
   COMMAND ${RESULT_FIND_OPT} --version
   USES_TERMINAL
-  DEPENDS RESULT_FIND_OPT)
+  DEPENDS RESULT_FIND_OPT
+)
 
-exec_program(${RESULT_FIND_OPT}
-  ARGS --version
+execute_process(
+  COMMAND ${RESULT_FIND_OPT} --version
   OUTPUT_VARIABLE LLVM_OPT_VERSION
-  )
+  RESULT_VARIABLE LLVM_OPT_RESULT
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
 
+if(NOT LLVM_OPT_RESULT EQUAL 0)
+  message(FATAL_ERROR "Failed to get opt version")
+endif()
 
 cmake_print_variables(LLVM_OPT_VERSION)
 
-exec_program(clang
-  ARGS --version
+execute_process(
+  COMMAND clang --version
   OUTPUT_VARIABLE CLANG_VERSION
-  )
+  RESULT_VARIABLE CLANG_RESULT
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
 
+if(NOT CLANG_RESULT EQUAL 0)
+  message(FATAL_ERROR "Failed to get clang version")
+endif()
 
 cmake_print_variables(CLANG_VERSION)
-
 
 set(REMCU_VM_PATH ${CMAKE_CURRENT_LIST_DIR}/../)
